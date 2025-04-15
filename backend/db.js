3c1481ad59a151f55b6beddae9f42574d48ed00a
SevanDvd <sevan.david2a@gmail.com>
2025-04-15 16:16:34 +0200

    init

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME   
+});
+
+
+db.connect((err) => {
+    if (err){
+        console.log('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as id ' + db.threadId);
+})
+
+module.exports = db;
