46dc8a704bccbcaff51f8957adb2c4ab5002d297
SevanDvd <sevan.david2a@gmail.com>
2025-04-21 10:32:50 +0200

    register / login

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME   
-});
-
-
-db.connect((err) => {
-    if (err){
-        console.log('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database as id ' + db.threadId);
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 })
 
-module.exports = db;
+db.connect((err)=>{
+    if(err){
+        console.log('Database connexion failed')
+        return err
+    }
+    console.log('Database connected')
+})
+
+module.exports = db
--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,51 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const db = require('./db')
-
+const express = require('express');
+const bcrypt = require('bcrypt')
+const db = require('./db.js')
 
 const app = express()
-
 app.use(express.json())
-const posts = [
-    {
-        username: "jacques",
-        title: "Post 1"
-    },
-    {
-        username: "benoit",
-        title: "Post 2"
-    }
-]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
-})
-
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res)=>{
     const username = req.body.username
     const password = req.body.password
-    console.log(username, password)
+    
+    if(!username || !password){
+        res.status(400).send('username and passwod are required')
+    }
 
-    db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err, result) => {
-        if (err) {
-            console.error(err)
-            res.status(500).send('Error registering user')
-        } else {
-            res.status(201).send('User registered successfully')
-            console.log(result)
-        }
-    })
+    try
+    {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const query = "INSERT INTO users (username, password) VALUES (?,?)"
+        await db.promise().execute(query, [username, hashedPassword])
+
+        res.status(201).send('user registered successfully')
+        console.log("user ", username, " registered successfully")
+    }
+    catch(err)
+    {
+        console.log('Error registering user :', err)
+        res.status(500).send('Internal server error')
+    }
 })
 
+app.post('/login',async (req, res)=>{
+    const username = req.body.username
+
+    try
+    {
+        const hash = await db.promise().execute('SELECT password FROM users WHERE username = ?',[username])
+        //checking passsword
+        const isLogin = await bcrypt.compare(req.body.password, hash[0][0].password)
+        console.log(isLogin)
+        !isLogin ? console.error("Connexion Failed") : console.log("Connected")
+    }
+    catch(err)          
+    {
+        console.log('Error logging user :', err)
+        res.status(500).send('Internal server error')
+    }
+
+})
 
 app.listen(3000)
